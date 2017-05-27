33c507622a319d6a16460767cc5d1f02ce3a1da2
Toge Wu <wtc@ekuaibao.com>
2017-05-27 19:02:35 +0800

    添加项目

---
+++ PreventDoubleClick/PreventDoubleClick/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PreventDoubleClick
+//
+//  Created by Toge Wu on 27/05/2017.
+//  Copyright © 2017 Toge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PreventDoubleClick/PreventDoubleClick/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PreventDoubleClick
+//
+//  Created by Toge Wu on 27/05/2017.
+//  Copyright © 2017 Toge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
